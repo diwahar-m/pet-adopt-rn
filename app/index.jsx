bb34c9baa7e3c43f737b9cf25d6a198988ba4738
diwahar-m <msdiwahar5@gmail.com>
2024-11-26 13:28:44 +0530

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Link href="/login">
+        <Text>Edit app/index.tsx to edit this syycreen.</Text>
+      </Link>
+    </View>
+  );
+}
