15812dea10553d291a0c152756754ca441f0d6e7
diwahar-m <msdiwahar5@gmail.com>
2025-01-15 18:58:48 +0530

    Chat feature linked with firebase database

--- app/pet-details/index.jsx
+++ app/pet-details/index.jsx
@@ -6,16 +6,18 @@ import {
   StyleSheet,
 } from "react-native";
 import React, { useEffect } from "react";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
 import PetInfo from "../../components/PetDetails/PetInfo";
 import PetSubInfo from "../../components/PetDetails/PetSubInfo";
 import AboutPet from "../../components/PetDetails/AboutPet";
 import OwnerInfo from "../../components/PetDetails/OwnerInfo";
 import Colors from "../../constants/Colors";
+import { db } from "../../config/FirebaseConfig";
 
 export default function PetDetails() {
   const pet = useLocalSearchParams();
   const navigation = useNavigation();
+  const router = useRouter();
 
   useEffect(() => {
     navigation.setOptions({
@@ -24,6 +26,45 @@ export default function PetDetails() {
       headerTitle: "",
     });
   }, []);
+
+  const InitiateChat = async () => {
+    const docId1 = user?.primaryEmailAddress?.emailAddress + "_" + pet?.email;
+    const docId2 = pet?.email + "_" + user?.primaryEmailAddress?.emailAddress;
+    const q = query(
+      collection(db, "Chat"),
+      where("id", "in", [docId1, docId2])
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data);
+      router.push({
+        pathname: "/chat",
+        params: { id: doc.id },
+      });
+    });
+
+    if (querySnapshot.docs?.length == 0) {
+      await setDoc(doc(db, "Chat", docId1), {
+        id: docId1,
+        users: [
+          {
+            email: user?.primaryEmailAddress?.emailAddress,
+            imageUrl: user?.imageUrl,
+            name: user?.fullName,
+          },
+          {
+            email: pet?.email,
+            imageUrl: pet?.userImage,
+            name: pet?.userName,
+          },
+        ],
+      });
+      router.push({
+        pathname: "/chat",
+        params: { id: docId1 },
+      });
+    }
+  };
 
   return (
     <View>
@@ -39,7 +80,7 @@ export default function PetDetails() {
         <View style={{ height: 70 }}></View>
       </ScrollView>
       <View style={styles.bottomContainer}>
-        <TouchableOpacity style={styles.adoptBtn}>
+        <TouchableOpacity onPress={InitiateChat} style={styles.adoptBtn}>
           <Text
             style={{
               textAlign: "center",
